44e37b442a9c7e7b29639c7e8efadbc106827606
20TwentyVizion <pyravizion@gmail.com>
2024-12-31 10:47:04 -0500

    Hide halo image

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,14 +10,15 @@ export default function Layout({
 }>) {
   return (
     <body className="bg-brand antialiased">
-      <div className="absolute inset-x-0 flex justify-center">
+      {/* Commenting out the halo image for now */}
+      {/* <div className="absolute inset-x-0 flex justify-center">
         <Image
           src={bgImg}
           alt=""
           className="w-full max-w-[1200px] mix-blend-screen"
           priority
         />
-      </div>
+      </div> */}
 
       <div className="isolate">
         <div className="mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-center py-2">
